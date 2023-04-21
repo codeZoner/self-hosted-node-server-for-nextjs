36d49f08074c7cf8f8d412c611bf478544252cdb
Fahim Hossain <fahims2015@gmail.com>
2023-04-22 02:48:40 +0600

    Self Hosting NextJS 12/13 with Node with SSL Termination.

---
+++ README.md
@@ -0,0 +1,68 @@
+
+# Self Hosting NextJS 12/13 with Node with SSL Termination
+There are a multiple approaches on how to deply NextJs Apps on self hosted server. This repo, we will be using a server.js file with next module to handle the requests. 
+
+--- 
+## 1. Requirements
+* A valid sub-domain to host the site
+* You will need a **valid pair of SSL cert & key files**.
+* Host Port to run the server.
+* Node.js JavaScript runtime environment
+* NextJS Version: 12 or 13. You can use an existing project or implement in on a basic installation
+
+## 2. Prepare your SSL certificate and key
+Copy and Paste the SSL Certicate & Key inside the **certs folder** located in the project's root directory. 
+* You can use any location but will need to update the path in the server.js file.
+* It is crucial to set proper permissions of the certificate & key files to maintain security and prevent unauthorized access. 
+  * Private Key ( *.pem, *.key): Set the permissions to -rw------- (600 in numeric notation). 
+  * Certificate (*.crt, *.pem, or *.cert): Set the permissions to -rw-r--r-- (644 in numeric notation).
+  
+## 3. Edit the **server.js** file
+Edit the server.js file to set up the following variables in accordance to your needs
+* **subdomain** Example: www.example.com
+* **port** Example: 443 (ensure that the port is not being used by any other service)
+* **sslConfig** (map the path to the cert/key files here)
+
+## 4. Install all the node packages
+* If you are running this sample template, just run 
+```bash
+  pnpm i 
+  # or
+  npm install --save
+  # or
+  yarn
+```
+
+* If you are implementing on an existing project, install the **ssl-root-cas** & **fs** packages using your preferred package manager
+```bash
+pnpm i ssl-root-cas fs
+# or
+npm install ssl-root-cas fs --save
+# or
+yarn add ssl-root-cas fs
+```
+
+## 5. Edit your **package.json** file
+- If running this standalone example, ignore this step 
+- If you are implementing on an existing project, then add the following two scripts to your package.json file.
+```json
+"scripts": {
+  "startStage": "NODE_ENV=development node server.js",
+  "startProd": "NODE_ENV=production node server.js"
+},
+```
+
+## 6. **Buid your Next App**
+Build your app by running -
+```bash
+pnpm build
+```
+## 7. Start the **Server** on Production or Staging and check on your browser
+
+```bash
+pnpm run startProd
+# or
+npm run startProd
+# or
+yarn run startProd
+```
---
+++ server.js
@@ -0,0 +1,143 @@
+/** 
+ * Title: Self Hosting a Next.js App with SSL 
+ * Description: Sample implementation of Custom server config
+ * NextJS Compatibility: 12 & 13
+ * Author: Fahim Hossain
+ * Designation: Founder & CEO at ARITS Limited
+ * Website: https://www.aritsltd.com
+ */
+
+
+
+/** 
+ * Import all required modules
+ */
+// If using non-ssl port replace https module with http module
+// const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const { parse } = require('url');
+const next = require('next');
+
+/**
+ * Set the sub domain for the site
+ */
+const subDomain = 'customnextserver.aritsltd.com';
+
+/**
+ * Using Default SSL port to access the site over 
+ * https://subdomain.domain.com
+ * Otherwise, if running on any other port (e.g. 8080),
+ * you will need to specify the port number in the URL
+ * Example - https://subdomain.domain.com:8080
+*/
+const port = 442;
+
+/*
+ *************************************************************
+ * SSL Certificate & Key 
+ * An Object Literal is used to store the SSL certificate & key
+ * Change the path to the location of your SSL certificate and key 
+ *************************************************************
+ *************************************************************
+ * For SSL certificate and key files, it is crucial to set the correct permissions
+ ************************************************************
+        * For SSL certificate and key files, it is crucial to set 
+        * proper permissions to maintain security and prevent unauthorized access. 
+        * Here are the recommended permissions for each type of file:
+        * Private Key (*.pem, *.key): The private key file should have strict permissions, as it's sensitive information. 
+        * Set the permissions to -rw------- (600 in numeric notation). 
+        * Certificate (*.pem, *.crt, or *.cert): Certificate files can have more relaxed permissions, as they do not contain sensitive information. 
+        * Set the permissions to -rw-r--r-- (644 in numeric notation). 
+ ************************************************************
+ */
+
+const sslConfig = {
+    cert: fs.readFileSync('./certs/cert.pem'),
+    key: fs.readFileSync('./certs/key.pem'),
+};
+
+
+/** 
+ * Set the environment variable for development or production
+ */
+const dev = process.env.NODE_ENV !== 'production';
+
+
+
+/**
+ * Initialize the Next.js app with the provided configuration
+ */
+const app = next({ dev, subDomain, port });
+
+/**
+ * Define the request handler for the Next.js App
+ */
+const handle = app.getRequestHandler();
+
+/**
+ * Prepare the Next.js app and start the server
+ */
+app.prepare().then(() => {
+    /** 
+     * Wrapping the request handling logic with the https.createServer() callback function. 
+     * This ensures that the request and response objects (req and res) are available when handling incoming requests. 
+     * If using non-ssl replace with http module
+    */
+    // http.createServer(async (req, res) => {
+    https.createServer(sslConfig, async (req, res) => {
+        try {
+            /** 
+             * Parse the URL, including the query portion
+             */
+            const parsedUrl = parse(req.url, true);
+            /** 
+             * Destructure the parsedUrl object to obtain the pathname and query properties
+            */
+            /** 
+             * Indent this if you are using custom routing logic
+             * read the next comment block for more details
+            */
+            await handle(req,res,parsedUrl);
+            
+            /** 
+             **************************************************************
+             * Indent the code below if you are using custom routing logic and 
+             * comment out the handle() function above
+            **************************************************************
+             * Route the request based on the pathname and redirect 
+             * Remove if you are not suing custom routing logic
+            */
+
+            // const { pathname, query } = parsedUrl;
+            // if (pathname === '/a') {
+            //     /**
+            //      * The app.render() function is a part of the Next.js framework,
+            //      * and it is used to render a specific page on the server side.
+            //      * This function is useful when you want to handle custom routing logic in the Next.js application.
+            //      */
+            //     await app.render(req, res, '/a', query);
+            // } else {
+            //     /**
+            //      * If pathname is not match, use the handle method to process the request
+            //      */
+            //     await handle(req, res, parsedUrl);
+            // }
+            
+            
+        } catch (err) {
+            /**
+             * Log the error and respond with an internal server error message
+             * Only print the log if the server is running on dev mode
+             */
+            if (dev) {
+                console.error('Error occurred handling', req.url, err);
+            }
+            res.statusCode = 500;
+            res.end('Internal server error');
+        }
+    }).listen(port, (err) => {
+        if (err) throw err;
+        console.log(`>> Initialized. Ready to handle requests at https://${subDomain}:${port}`);
+    });
+})
